b69f12867e4b3a16da3ca9050255e5203d233471
Connor Brown <connorbrown1@usf.edu>
2025-06-27 21:47:35 -0400

    I built the Manager class that inherited from the Employee and added the teamSize property.

--- cc_6.js
+++ cc_6.js
@@ -8,3 +8,13 @@ describe() {
         return `${this.name} works in ${this.department}`;
     }
 }
+//Step 3: Create a subclass
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+    describe() {
+        return `${this.name} and manages a team of ${this.department} department with a team of ${this.teamSize.Size}.`;
+    }
+}
